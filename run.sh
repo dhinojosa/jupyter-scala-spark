d22e5e8106b6605b13e42e79f71ed939c10a235d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-07-24 10:38:06 -0600

    updated with machine learning and that completes the drafts of exercises

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ docker run -it --rm \
   -p 8888:8888 -p 4040:4040 \
   --cpus=2.0 --memory=2000M \
   -v "$PWD":/home/jovyan \
+  -v "$PWD/tmp":/tmp \
   "$@" \
-  jupyter/all-spark-notebook
+  jupyter/all-spark-notebook:7a3e968dd212
